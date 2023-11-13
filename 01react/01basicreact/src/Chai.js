85af67a2849703b164d83ff8bdccc2ff77c00d39
ayush-maddhesiya <ayushkumarmaddhesiya@gmail.com>
2023-11-13 09:30:58 +0530

    modifed filed of both

---
+++ 01react/01basicreact/src/Chai.js
@@ -0,0 +1,9 @@
+function Chai() {
+    return(
+        <h1>
+            chai yeh coffee || today.
+        </h1>
+    )
+}
+
+export default Chai;
---
+++ 01react/01vitebasic/src/chai.jsx
@@ -0,0 +1,7 @@
+function Chai(){
+    return(
+        <h1>hello eorlf</h1>
+    )
+}
+
+export default Chai;
